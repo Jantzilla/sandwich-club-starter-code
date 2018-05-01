0e5c5642e4fb286db587db5c074393657ae506ee
Jantz <jantzillac.93@gmail.com>
2018-05-01 18:47:37 -0500

    Completed Json Parsing, connecting json values to the Sandwich object, and populating the UI.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,17 +3,22 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import org.json.JSONException;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+    Sandwich sandwich = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +41,11 @@ public class DetailActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+        try {
+            sandwich = JsonUtils.parseSandwichJson(json);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         if (sandwich == null) {
             // Sandwich data unavailable
             closeOnError();
@@ -57,6 +66,20 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void populateUI() {
+        TextView alsoKnownAsLabel = findViewById(R.id.also_known_label);
+        TextView alsoKnownAs = findViewById(R.id.also_known_tv);
+        TextView ingredients = findViewById(R.id.ingredients_tv);
 
+        if(sandwich.getAlsoKnownAs().isEmpty()){                      //Removes nickname TextViews if no "alsoKnownAs" value
+            alsoKnownAsLabel.setVisibility(View.GONE);
+            alsoKnownAs.setVisibility(View.GONE);
+        }
+        alsoKnownAs.setText(sandwich.getAlsoKnownAs().toString());
+
+        ingredients.setText(sandwich.getIngredients().toString());
+        TextView placeOfOrigin = findViewById(R.id.origin_tv);
+        placeOfOrigin.setText(sandwich.getPlaceOfOrigin());
+        TextView description = findViewById(R.id.description_tv);
+        description.setText(sandwich.getDescription());
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,61 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
-        return null;
+    public static Sandwich parseSandwichJson(String json) throws JSONException {
+        final JSONObject obj = new JSONObject(json);
+        final JSONObject innerObj = obj.getJSONObject("name");
+
+        final String name = innerObj.getString("mainName");
+        final String description = obj.getString("description");
+        final String image = obj.getString("image");
+        final JSONArray ingredients = obj.getJSONArray("ingredients");
+
+        final String placeOfOrigin;
+        ArrayList<String> listKnownAs = new ArrayList<String>();
+        JSONArray alsoKnownAs = null;
+        Sandwich sandwich = new Sandwich();
+
+
+        if(innerObj.has("alsoKnownAs")) {
+            alsoKnownAs = innerObj.getJSONArray("alsoKnownAs");
+            int len = alsoKnownAs.length();
+            for (int i = 0; i < len; i++) {
+                listKnownAs.add(alsoKnownAs.get(i).toString());
+            }
+            sandwich.setAlsoKnownAs(listKnownAs);
+        }
+        else
+            sandwich.setAlsoKnownAs(listKnownAs);
+
+
+        placeOfOrigin = obj.getString("placeOfOrigin");
+        if(placeOfOrigin.equals("")) {
+            sandwich.setPlaceOfOrigin("Unknown");
+        }
+        else
+            sandwich.setPlaceOfOrigin(placeOfOrigin);
+
+        ArrayList<String> listIngredients = new ArrayList<String>();
+        if (ingredients != null) {
+            int len = ingredients.length();
+            for (int i=0;i<len;i++){
+                listIngredients.add(ingredients.get(i).toString());
+            }
+        }
+
+        sandwich.setMainName(name);
+        sandwich.setDescription(description);
+        sandwich.setImage(image);
+        sandwich.setIngredients(listIngredients);
+
+        return sandwich;
     }
 }
